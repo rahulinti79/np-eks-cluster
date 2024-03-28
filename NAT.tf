ff46a1757256d3e95184354ed564f1ba49482314
rahulinti79 <114361901+rahulinti79@users.noreply.github.com>
2024-03-27 23:17:04 -0500

    Update NAT.tf

--- NAT.tf
+++ NAT.tf
@@ -9,11 +9,11 @@ resource "aws_nat_gateway" "gw1" {
 }
 
 resource "aws_nat_gateway" "gw2" {
-    allocation_id = aws_eip.nat1.id
+    allocation_id = aws_eip.nat2.id
     subnet_id = aws_subnet.dev-np-eks-pub2.id
 
     tags = {
-        Name = "NAT 1"
+        Name = "NAT 2"
     }
   
 }
