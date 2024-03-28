d1774270f5f8e96ea54852ef9e34dc8ab3e67da2
rahulinti79 <114361901+rahulinti79@users.noreply.github.com>
2024-03-27 22:53:30 -0500

    Add files via upload

---
+++ vpc.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "main" {
+    cidr_block = "192.168.0.0/16"
+    instance_tenancy = "default"
+    #Required by EKS
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    assign_generated_ipv6_cidr_block = false 
+
+    tags ={
+        Name = "np-dev-eks"
+    }   
+  
+}
+
+output "vpc_id" {
+    value = "aws_vpc.main.id"
+    description = "vpc id"
+    sensitive = false
+} 
